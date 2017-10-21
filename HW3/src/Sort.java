d3549d31af9b977cb995615cf44763a0de46872b
Emily Tran <trane@oxy.edu>
2017-10-21 15:09:57 -0700

    Quick Sort Added

--- HW3/src/Sort.java
+++ HW3/src/Sort.java
@@ -4,13 +4,42 @@ import java.util.Random;
 public class Sort {
     public static void main(String args[]){
         Random r = new Random();
-        int[] arr = new int[30];
-        for(int l = 0; l < 30; l++){
+        int[] arr = new int[50];
+        for(int l = 0; l < 50; l++){
             arr[l] = r.nextInt(100);
         }
-        System.out.println(Arrays.toString(mergeSort(arr)));
+        quickSort(arr,0,arr.length-1);
+        System.out.print(Arrays.toString(arr));
     }
 
+    public static void quickSort(int[] a, int low, int high){
+        if(low < high) { //if there are elements left
+            int pivot = partition(a, low, high);
+
+            quickSort(a, low, pivot - 1); //partition the left of the pivot
+            quickSort(a, pivot + 1, high); //partition the right of the pivot
+        }
+    }
+
+    public static int partition(int[] a, int low, int high){
+        int pivot = a[high]; //setting the top element as the pivot
+        int i = low - 1;
+
+        for(int j = low; j < high; j++){ //check every element and compare it to the pivot
+            if(a[j] <= pivot){  //if the current element is smaller than the pivot
+                i++;
+                swap(a,i,j); //put the current element to the left of the pivot
+            }
+        }
+        swap(a,i+1,high);
+        return i+1; //return the pivot
+    }
+
+    public static void swap(int[] a, int b, int c){
+        int temp = a[b];
+        a[b] = a[c];
+        a[c] = temp;
+    }
 
     public static int[] mergeSort(int[] a){
         if(a.length > 1) {
@@ -67,7 +96,7 @@ public class Sort {
         return a;
     }
 
-    public static int[] insertionSort(int[] a){
+    public static int[] insertionSort(int[] a){ //
         for(int i = 0; i < a.length-1; i++){
             int key = a[i + 1];
 
@@ -80,7 +109,7 @@ public class Sort {
         return a;
     }
 
-    public static int[] bubbleSort(int[] a){
+    public static int[] bubbleSort(int[] a){ //bubble largest element to the end
         boolean sorted = false;
         int count = 1;
 
